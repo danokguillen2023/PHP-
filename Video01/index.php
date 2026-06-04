a422177cd02e7e0f8d836c9efdd04c1812350afa
Daniel <danokguillen2023@gmail.com>
2026-06-03 19:36:28 -0600

    funcionamiento de php con xampp

---
+++ Video01/index.php
@@ -0,0 +1,10 @@
+<?php
+    // Inicia el código PHP
+
+    // Muestra el texto "Hola mundo" en la pantalla
+    echo "Hola mundo"; 
+    
+    // Finaliza el código PHP
+?>
+
+// uso de xampp para ver en un servidor local como funciona la app 
