d9af2992078716a706074deb42600add6fbab98a
danokguillen2023 <danokguillen2023@gmail.com>
2026-06-04 13:17:09 -0600

    Operadores matematicos

---
+++ Video05/oMatematicos.php
@@ -0,0 +1,17 @@
+<?php
+
+    $variable = 34;
+
+    $variable_negativa = -$variable;
+
+    $a =  2;
+    $b = 5;
+
+    $c = $a + $b;
+
+    echo $c;
+    echo "<br></br>";
+    echo $variable_negativa;
+    echo "<br></br>";
+    echo 4 * 5 - 6 + 4 * 2 + 1 * 0;
+?>
