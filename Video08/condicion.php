0ef7c85972ee09c7f695fd1bdd8240fef533f7d0
danokguillen2023 <danokguillen2023@gmail.com>
2026-06-04 16:14:16 -0600

    Estructura condicional

---
+++ Video08/condicion.php
@@ -0,0 +1,15 @@
+<?php
+
+    $a = 5;
+    $b = 3;
+
+    if ($a > $b){
+        echo "El valor de a es mayor que el valor de b";
+        }elseif($a<$b)
+        {
+        echo "El valor de a es menor que el valor de b";
+    }else
+    {
+        echo "El valor de a es igual que el valor de b";
+    }
+?>
---
+++ Video08/estructuraCondicion.php
@@ -0,0 +1,19 @@
+<?php
+
+
+    $a = 5;
+    $b = 3;
+
+    echo "Primer parrafo <br>";
+
+    if($a < $b){
+        
+        echo "Segundo parrafo <br>";
+        }else{
+        echo "Segundo parrafo editado <br>";
+
+    }
+
+
+    echo "Tercer parrafo <br>";
+?>
