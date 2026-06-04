cfefe4cb6a41745e14d3303f35d3061211389c4c
danokguillen2023 <danokguillen2023@gmail.com>
2026-06-04 12:42:51 -0600

    Tipos de datos en php

---
+++ Video04/tiposDeDatos.php
@@ -0,0 +1,32 @@
+<?php
+    // php no se necesita especificar como en otros lenguajes que tipo de dato numero php lo define 
+    $numero = 123.123;
+    $cadena = 'Hola mundo';
+    $comillas = 'hola mundo mi nombre es "Daniel"';
+    $comillas_dos = "Hola mundo mi nombre es 'Guillen'";
+    $usoComillas= "Ejemplo para usar caracteres: 'Daniel' y tengo \"24\" años de edad ";
+    $usoComillasDos= "Ejemplo para usar caracteres: 'Daniel' \$es y tengo '24' años de edad ";
+
+    $bool = true;
+    // $bool = false;
+
+
+
+    echo "El numero decimal es: " . $numero;
+    echo "<br></br>";
+    echo "La cadena almacenada es: " . $cadena;
+    echo "<br></br>";
+    echo $comillas;
+    echo "<br></br>";
+    echo $comillas_dos;
+    echo "<br></br>";
+    echo $usoComillas;
+    echo "<br></br>";
+    echo $usoComillasDos;
+    echo "<br></br>";
+    echo $bool;
+    echo "<br></br>";
+    echo $bool;
+
+?>
+
