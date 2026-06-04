b0f70c5b4f5c2ef7f08c04a0ef3c1cf4c1eaf288
danokguillen2023 <danokguillen2023@gmail.com>
2026-06-04 15:12:34 -0600

    operadores logicos y de comparacion php

---
+++ Video06/oLogicosYdeComp.php
@@ -0,0 +1,30 @@
+<?php
+
+    $a = 10;
+    $b = 1;
+
+    $c= true;
+    $d= true;
+
+    echo $a == $b;
+    echo "<br></br>";
+    var_dump($a == $b);
+    echo "<br></br>";
+    var_dump($a <> $b);
+    echo "<br></br>";
+    var_dump($a != $b);
+    echo "<br></br>";
+    var_dump($a > $b);
+    echo "<br></br>";
+    var_dump($a < $b);
+    echo "<br></br>";
+    var_dump($a <= $b);
+    echo "<br></br>";
+    var_dump($a >= $b);
+    
+    echo "<br></br>";
+    var_dump($c >= $d);
+    
+
+
+?>
